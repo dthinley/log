aa2226f35008e1d97d29812b0ec18934be68dd22
DT <dthinley@yahoo.com>
2017-10-16 10:53:09 -0700

    Update news.py

--- news.py
+++ news.py
@@ -11,12 +11,12 @@ class Log:
         except Exception as e:
             print(e)
 
-    def execute_query(self, query):
+    def get_query(self, query):
         self.cursor.execute(query)
         return self.cursor.fetchall()
 
-    def solve(self, task, query, suffix='views'):
-        result = self.execute_query(query)
+    def print_result(self, task, query, suffix='views'):
+        result = self.get_query(query)
         print(task)
         for i in range(len(result)):
             print i + 1, result[i][0], result[i][1], suffix
@@ -24,27 +24,29 @@ class Log:
     def exit(self):
         self.db.close()
 
-# First query
+# First Task
 task_1 = 'Q 1. What are the most popular three articles of all time?'
 query_1 = ("""SELECT title, count(path) FROM articles, log
                 WHERE status LIKE '200%'
                 AND  CONCAT('/article/', articles.slug) = log.path
                 GROUP BY title
                 ORDER BY count(path) DESC
-                LIMIT 3""")
-# Second query
+                LIMIT 3
+                """)
+# Second Task
 task_2 = 'Q 2. Who are the most popular article authors of all time?'
 query_2 = ("""SELECT authors.name, COUNT(*)
                 FROM articles INNER JOIN authors ON articles.author = authors.id
                 INNER JOIN log ON CONCAT('/article/', articles.slug) = log.path
                 WHERE  log.status LIKE '200%'
                 GROUP BY authors.name
-                ORDER BY COUNT(log.path) DESC""")
-# Third query
+                ORDER BY COUNT(log.path) DESC
+                """)
+# Third Task
 task_3 = 'Q 3. On which days did more than 1% of requests lead to errors?'
 query_3 = ("""SELECT date, rate
                 FROM (SELECT to_char(TIME, 'Mon DD, YYYY') AS date,
-                round(cast(100*sum(CASE WHEN status!='200 OK' THEN 1
+                round(cast(100*sum(CASE WHEN status!='200 OK' THEN 1 
                 ELSE 0
                 END)::float/sum(CASE
                 WHEN status='200 OK' THEN 1
@@ -52,15 +54,17 @@ query_3 = ("""SELECT date, rate
                 END) AS numeric), 1) AS rate
                 FROM log
                 GROUP BY date) AS err
-                WHERE rate>1""")
+                WHERE rate>1
+                """)
 
+# Result and print out
 if __name__ == '__main__':
     log = Log()
     print("-" * 60)
-    log.solve(task_1, query_1)
+    log.print_result(task_1, query_1)
     print("-" * 60)
-    log.solve(task_2, query_2)
+    log.print_result(task_2, query_2)
     print("-" * 60)
-    log.solve(task_3, query_3, '% error')
+    log.print_result(task_3, query_3, '% error')
     print("-" * 60)
     log.exit()
